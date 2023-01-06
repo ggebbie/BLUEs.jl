522a914928447a44407bfaaff149adb95d8e48fe
G Jake Gebbie <ggebbie@whoi.edu>
2023-01-06 17:09:18 -0500

    just determined left pseudoinverse method

--- src/BLUEs.jl
+++ src/BLUEs.jl
@@ -1,5 +1,58 @@
 module BLUEs
 
-# Write your package code here.
+using LinearAlgebra, Statistics, Unitful, UnitfulLinearAlgebra, Measurements
+
+export solve, show
+
+import Base: show, getproperty, propertynames
+
+#struct Measurement{T<:AbstractFloat} <: AbstractFloat
+
+struct Estimate{Tv <: Number,TC <: Number} 
+    v :: AbstractVector{Tv}
+    C :: AbstractMatrix{TC}
+end
+
+function show(io::IO, mime::MIME{Symbol("text/plain")}, x::Estimate{<:Number})
+    summary(io, x); println(io)
+    println(io, "Estimate and 1σ uncertainty")
+    show(io, mime, x.x)
+    # println(io, "\nsingular values:")
+    # show(io, mime, F.S)
+    # println(io, "\nV (right singular vectors):")
+    # show(io, mime, F.V)
+end
+
+"""
+    function getproperty(x::Estimate, d::Symbol)
+
+# Fields of Estimate
+- `x::Vector{Measurement}`: Estimate and 1σ uncertainty
+- `σ::Vector{Number}`: 1σ uncertainty
+- `v::Vector{Number}`: central value of estimate
+- `C::Matrix{Number}`: estimate uncertainty matrix
+"""
+function getproperty(x::Estimate, d::Symbol)
+    if d === :σ
+        return .√diag(x.C)
+    elseif d === :x
+        return measurement.(x.v,x.σ)
+        #return x.v .± x.σ
+    else
+        return getfield(x, d)
+    end
+end
+
+propertynames(x::Estimate) = (:x, :σ, fieldnames(typeof(x))...)
+
+# Code to make some property names private
+#Base.propertynames(x::Estimate, private::Bool=false) =
+#    private ? (:U, :U⁻¹, :V, :V⁻¹,  fieldnames(typeof(F))...) : (:U, :U⁻¹, :S, :V, :V⁻¹)
+
+function solve(y::AbstractVector,E::AbstractMatrix,Cnn⁻¹::AbstractMatrix)
+    CE = Cnn⁻¹*E
+    ECE = transpose(E)*CE
+    return Estimate( ECE \ (transpose(CE)*y), inv(ECE))
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,47 @@
-using BLUEs
+using Revise, BLUEs
 using Test
+using LinearAlgebra, Statistics, Unitful, UnitfulLinearAlgebra, Measurements
 
 @testset "BLUEs.jl" begin
-    # Write your tests here.
+
+    const permil = u"permille"; const K = u"K"; const	K² = u"K^2"; m = u"m"; s = u"s"; MMatrix = BestMultipliableMatrix
+    ENV["UNITFUL_FANCY_EXPONENTS"] = true
+    MMatrix = BestMultipliableMatrix
+
+    @testset "just determined left pseudoinverse" begin
+        M = 2
+        N = 2
+        σₓ = rand()
+        # exact = false to work
+        E = MMatrix(randn(M,N),fill(m,M),fill(m,N),exact=true)
+        Cnn⁻¹ = Diagonal(fill(σₓ^-1,N),unitdomain(E).^-1,unitdomain(E),exact=true)
+        x = randn(N)m
+        y = E*x
+        x̃ = solve(y,E,Cnn⁻¹)
+        @test x ≈ x̃.v
+    end
+
+    @testset "trend analysis" begin
+
+        M = 10  # number of obs
+        t = collect(0:1:M-1)s
+
+        a = randn()m # intercept
+        b = randn()m/s # slope
+
+        y = a .+ b.*t .+ randn(M)m
+
+        E = MMatrix(hcat(ones(M),ustrip.(t)),fill(m,M),[m,m/s],exact=true)
+        Cnn⁻¹ = Diagonal(fill(1.0,M),fill(m^-1,M),fill(m,M),exact=true)
+
+        #solve(y,E,Cnn⁻¹=Cnn⁻¹)
+
+
+        # start here
+        x̃ = solve(y,E,Cnn⁻¹)
+        E⁺ = (E'*(W⁻¹*E)) \ (E'*W⁻¹)
+        
+    end
+    
+
 end
