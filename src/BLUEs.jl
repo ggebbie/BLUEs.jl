867ff0da965c6ac0dfadfc2364759468e08d4f93
G Jake Gebbie <ggebbie@whoi.edu>
2024-06-18 09:28:40 -0400

    split unitful material

--- src/BLUEs.jl
+++ src/BLUEs.jl
@@ -77,6 +77,7 @@ end
 
 #include("dim_estimate.jl")
 include("base.jl")
+include("unitful.jl")
 include("blockdim.jl")
 
 """
@@ -534,36 +535,6 @@ function impulseresponse(funk::Function,x,args...)
     else
         return UnitfulMatrix(Ep,vec(unit.(y₀)),vec(unit.(x)))
     end
-end
-
-function UnitfulMatrix_from_input_output(Eu,y,x)
-    if length(x) == 1 && length(y) == 1
-        return UnitfulMatrix(ustrip.(Eu),[unit(y)],[unit(x)])
-    elseif length(y) == 1
-        return UnitfulMatrix(ustrip.(Eu),[unit.(y)],vec(unit.(x)))
-    elseif length(x) == 1
-        return UnitfulMatrix(ustrip.(Eu),vec(unit.(y)),[unit(x)])
-    else
-        return UnitfulMatrix(ustrip.(Eu),vec(unit.(y)),vec(unit.(x)))
-    end
-end
-
-function expectedunits(y,x)
-    Eunits = Matrix{Unitful.FreeUnits}(undef,length(y),length(x))
-    for ii in eachindex(y)
-        for jj in eachindex(x)
-            if length(y) == 1 && length(x) ==1
-                Eunits[ii,jj] = unit(y)/unit(x)
-            elseif length(x) == 1
-                Eunits[ii,jj] = unit.(y)[ii]/unit(x)
-            elseif length(y) ==1
-                Eunits[ii,jj] = unit(y)/unit.(x)[jj]
-            else
-                Eunits[ii,jj] = unit.(y)[ii]/unit.(x)[jj]
-            end
-        end
-    end
-    return Eunits
 end
 
 """
---
+++ src/unitful.jl
@@ -0,0 +1,29 @@
+function UnitfulMatrix_from_input_output(Eu,y,x)
+    if length(x) == 1 && length(y) == 1
+        return UnitfulMatrix(ustrip.(Eu),[unit(y)],[unit(x)])
+    elseif length(y) == 1
+        return UnitfulMatrix(ustrip.(Eu),[unit.(y)],vec(unit.(x)))
+    elseif length(x) == 1
+        return UnitfulMatrix(ustrip.(Eu),vec(unit.(y)),[unit(x)])
+    else
+        return UnitfulMatrix(ustrip.(Eu),vec(unit.(y)),vec(unit.(x)))
+    end
+end
+
+function expectedunits(y,x)
+    Eunits = Matrix{Unitful.FreeUnits}(undef,length(y),length(x))
+    for ii in eachindex(y)
+        for jj in eachindex(x)
+            if length(y) == 1 && length(x) ==1
+                Eunits[ii,jj] = unit(y)/unit(x)
+            elseif length(x) == 1
+                Eunits[ii,jj] = unit.(y)[ii]/unit(x)
+            elseif length(y) ==1
+                Eunits[ii,jj] = unit(y)/unit.(x)[jj]
+            else
+                Eunits[ii,jj] = unit.(y)[ii]/unit.(x)[jj]
+            end
+        end
+    end
+    return Eunits
+end
