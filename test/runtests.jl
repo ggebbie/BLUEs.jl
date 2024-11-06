c931f75f05b43155875d132e89d2d72ee37a7af3
G Jake Gebbie <ggebbie@whoi.edu>
2024-11-06 17:04:12 -0500

    non-uniform water-mass problem: no `Unitful`
    non-uniform Unitful matrices too difficult to broadcast

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,7 +32,8 @@ includet("test_functions.jl")
         include("test_estimate.jl")
         include("test_algebraic_arrays.jl")
         include("test_dimensional_data.jl") ## next step: HERE
-        #include("test_unitful_linear_algebra.jl")
+        include("test_unitful_linear_algebra.jl")
+        #include("test_DD_ULA.jl")
     end
 
 end
