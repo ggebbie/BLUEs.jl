ab8fd00e3e34f499991a5b44d0e1632c6c7de969
G Jake Gebbie <ggebbie@whoi.edu>
2025-12-15 23:51:07 +0000

    remove comments

--- ext/DimensionalDataExt.jl
+++ ext/DimensionalDataExt.jl
@@ -141,7 +141,6 @@ function BLUEs.convolve(x::VectorArray, M::AbstractDimArray, t::Number)
     return sum([M[ii,:] ⋅ x[Near(t-ll),:] for (ii,ll) in enumerate(lags)])
 end
 
-#function convolve(x::AbstractDimArray,M::AbstractDimArray,Tx::Union{Ti,Vector})
 function BLUEs.convolve(x::VectorArray,M::AbstractDimArray,Tx::Union{Ti,Vector})
     if ndims(M) == 2 
         return VectorArray(DimArray([convolve(x,M,Tx[tt]) for (tt,yy) in enumerate(Tx)],Tx))
@@ -168,7 +167,6 @@ function BLUEs.convolve(P::MatrixArray, M::AbstractDimArray, Tx::Union{Ti,Vector
     T2 = typeof(parent(convolve(first(P),M,Tx)))
     Pyx = Array{T2}(undef,size(P))
     for i in eachindex(P)
-        #Pyx[i] = parent(parent(convolve(P[i],M,Tx,coeffs)))
         Pyx[i] = parent(convolve(P[i],M,Tx))
     end
     return MatrixArray(DimArray(Pyx,domaindims(P)))
@@ -181,7 +179,6 @@ function BLUEs.convolve(P::MatrixArray,M::AbstractDimArray)
     println(T2)
     Pyx = Array{T2}(undef,size(P))
     for i in eachindex(P)
-        #Pyx[i] = convolve(P[i],M)
         Pyx[i] = first(parent(convolve(P[i],M)))
     end
     return AlgebraicArray(Pyx,RowVector(["1"]),rangedims(P))
@@ -190,22 +187,17 @@ end
 # basically repeats previous function: any way to simplify?
 function BLUEs.convolve(P::MatrixArray,M::AbstractDimArray,coeffs::DimVector)
     T2 = typeof(first(parent(convolve(first(P),M,coeffs))))
-    #T2 = typeof(convolve(first(P),M,coeffs))
     Pyx = Array{T2}(undef,size(P))
     for i in eachindex(P)
         #        Pyx[i] = convolve(P[i],M,coeffs)
         Pyx[i] = first(parent(convolve(P[i],M,coeffs)))
     end
-    #return DimArray(Pyx,dims(P))
     return transpose(VectorArray(DimArray(Pyx,rangedims(P))))
-    #return AlgebraicArray(Pyx,RowVector(["1"]),rangedims(P))
-    #return MatrixArray(DimArray(Pyx,(RowVector(["1"]),rangedims(P))))
 end
 
 function BLUEs.convolve(x::VectorArray, M::AbstractDimArray, coeffs::DimVector) 
     statevars = dims(x,3) # equal to rangedims(x)[3]
     vals = sum([convolve(x[:,:,At(s)], M)  * coeffs[At(s)] for s in statevars])
-    #return sum([convolve(x[:,:,At(s)], M)  * coeffs[At(s)] for s in statevars])
     (vals isa Number) ? (return VectorArray(DimArray([vals],first(rangedims(x))))) : (return VectorArray(AlgebraicArray(vals,first(rangedims(x)))))
 end
 
