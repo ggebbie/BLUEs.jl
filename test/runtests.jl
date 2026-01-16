c7468aeb3be31a0dd7b079665c17bf6568bee975
G Jake Gebbie <ggebbie@whoi.edu>
2026-01-16 15:19:27 -0500

    fix missing macro

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,13 +17,20 @@ includet("test_functions.jl")
         include("test_dimensional_data.jl")
     end
 
-    @testset "extensions with units" begin
-        global K = u"K"; global K² = u"K^2"; global m = u"m"; global s = u"s";
-        global permil = Unitful.FixedUnits(u"permille")
+    @testset "extensions with Unitful" begin
+        using Unitful
+        K = u"K"; K² = u"K^2";  m = u"m"; s = u"s";
+        permil = Unitful.FixedUnits(u"permille")
         include("test_estimate_unitful.jl")
         include("test_unitful_linear_algebra.jl")
         include("test_algebraic_arrays_unitful.jl")
         include("test_dimensional_data_unitful.jl")
         include("test_DD_AA.jl")
     end
+
+    # @testset "extensions with DynamicQuantities" begin
+    #     global K = u"K"; global K² = u"K^2"; global m = u"m"; global s = u"s";
+    #     global permil = Unitful.FixedUnits(u"permille")
+    #     include("test_estimate_dynamicquantities.jl")
+    # end
 end
